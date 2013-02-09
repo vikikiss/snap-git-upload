ed810d41383f6acd2444123beb9159571e4d819b
Viktoria Kiss <kviki@elte.hu>
2013-02-09 22:44:49 +0800

    Cleanup

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Main where
+module Main (main) where
 
 import           Control.Applicative
 import           Snap.Core
@@ -43,14 +43,12 @@ main = do
 site :: Chan Job -> Snap ()
 site chan = foldr1 (<|>)
             [ ifTop (serveFile "static/main.html")
-            , route [ ("process", processHandler)
-                    , ("login", serveFile "static/login.html")
-                    , ("loginprocess", loginProcessHandler)
-                    , ("listingFiles", listFilesHandler)
-                    , ("fileUpload", serveFile "static/fileUpload.html")
-                    , ("txtfileprocess", getUsername >>= txtFileHandler chan)
+            , route [ ("login", serveFile "static/login.html")
+                    , ("logout", logout)
+                    , ("doLogin", login)
+                    , ("upload", getUsername >> serveFile "static/upload.html")
+                    , ("doUpload", getUsername >>= upload chan)
                     ]
-            , dir "upload" $ getUsername >> serveFile "static/upload.html"
             , dir "files" (serveDirectory "files/")
             ]
 
@@ -62,8 +60,8 @@ getUsername = forceJust maybeLoginCookie <|> redirect "/login"
         ipAddress <- getsRequest rqRemoteAddr
         return $ isValidLoginCookie ipAddress $ cookieValue cookie
 
-txtFileHandler :: Chan Job -> ByteString -> Snap ()
-txtFileHandler chan username = do
+upload :: Chan Job -> ByteString -> Snap ()
+upload chan username = do
     handleFileUploads "files/txtFiles" defaultUploadPolicy allowTxt $ \parts -> do
         case find (\(pi, _) -> partFieldName pi == "file") parts of
             Just (pi, Right tmpFileName) -> do
@@ -92,79 +90,41 @@ forceJust m = do
         Just x -> return x
         Nothing -> pass
 
-extForContentType :: ByteString -> Maybe FilePath
-extForContentType "image/jpeg" = Just ".jpg"
-extForContentType "image/png" = Just ".png"
-extForContentType _ = Nothing
-
-allowImages :: PartInfo -> PartUploadPolicy
-allowImages pi = if isJust (extForContentType (partContentType pi))
-                 then allowWithMaximumSize maxBound
-                 else disallow
-
 allowTxt :: PartInfo -> PartUploadPolicy
 allowTxt pi = if "text/" `BS.isPrefixOf` (partContentType pi)
               then allowWithMaximumSize maxBound
               else disallow
 
-processHandler :: Snap ()
-processHandler = do
-    handleFileUploads "files" defaultUploadPolicy allowImages $ \parts -> do
-        case find (\(pi, _) -> partFieldName pi == "file") parts of
-            Just (pi, Right tmpFileName) -> do
-                let ext = fromMaybe ".dat" $ extForContentType (partContentType pi)
-                bs <- liftIO $ BS.readFile tmpFileName
-                fileName <- getParam "fileName"
-                fileName' <- case fileName of
-                    Nothing -> do
-                        writeText "<p>Error</p>"
-                        modifyResponse $ setResponseStatus 500 "Internal Server Error"
-                        finishWith =<< getResponse
-                    Just fn -> return $ if BS.null fn then
-                                          BS.toString $ fromJust (partFileName pi)
-                                        else
-                                          takeBaseName (BS.toString fn) <.> ext
-                liftIO $ BS.writeFile ("files" </> fileName') bs
+login :: Snap ()
+login = do
+    username <- forceJust $ getPostParam "username"
+    password <- forceJust $ getPostParam "password"
+    guard $ username == "viki" && password == "foo"
 
-                modifyResponse (setContentType "text/html")
-                writeText $ uploadedMessage fileName' (BS.length bs)
-            _ -> writeBS $ fromString $ show parts
+    ipAddress <- getsRequest rqRemoteAddr
+    modifyResponse $ addResponseCookie $ toLoginCookie username ipAddress
+    redirect "/"
 
-
-
-uploadedMessage :: FilePath -> Int -> Text
-uploadedMessage fileName fileSize =
-    T.unlines [ "<p>You have uploaded a file</p>"
-              , "<p>" <> T.unwords ["Size:", fromString $ show fileSize, "B"] <> "</p>"
-              , "<a href='http://google.com/'>link</a>"
-              ]
-
-listFilesHandler :: Snap ()
-listFilesHandler = do
-    filePaths <- liftIO $ getDirectoryContents "files"
-    let fps = filter (\fp -> fp /= "." && fp /= "..") $ filePaths
-    forM_ (sort fps) (\filePath -> do
-                                 writeText ("<p><a href='files/" `mappend` fromString filePath `mappend` "'>" `mappend` fromString filePath `mappend` "</a></p>"))
-    -- writeBS $ fromString filePath)
-
-loginProcessHandler :: Snap ()
-loginProcessHandler = do
-    username <- getPostParam "username"
-    password <- getPostParam "password"
-    case username of
-        Nothing -> return ()
-        Just username -> case password of
-            Nothing -> return ()
-            Just password -> do
-                when (username == "viki" && password == "foo") $ do
-                    ipAddress <- getsRequest rqRemoteAddr
-                    modifyResponse $ addResponseCookie $ toLoginCookie username ipAddress
-                    redirect "/"
+logout :: Snap ()
+logout = do
+    modifyResponse $ addResponseCookie noLoginCookie
+    redirect "/"
 
 toLoginCookie :: ByteString -> ByteString -> Cookie
 toLoginCookie username ipAddress = Cookie
     { cookieName = "token"
     , cookieValue = BS.intercalate ":" [username, ipAddress, toToken username ipAddress]
+    , cookieExpires = Nothing
+    , cookieDomain = Nothing
+    , cookiePath = Nothing
+    , cookieSecure = False
+    , cookieHttpOnly = True
+    }
+
+noLoginCookie :: Cookie
+noLoginCookie = Cookie
+    { cookieName = "token"
+    , cookieValue = BS.empty
     , cookieExpires = Nothing
     , cookieDomain = Nothing
     , cookiePath = Nothing
